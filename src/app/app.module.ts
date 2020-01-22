b1bb55b2b096da8239095443777e4f3b93cf2e1f
Xetro <golob.matevz@gmail.com>
2020-01-22 20:54:39 +0100

    Create project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
+import { CompanyComponent } from './company/company.component';
+import { CompaniesListComponent } from './companies-list/companies-list.component';
+import { CompanyDetailComponent } from './company-detail/company-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+    CompanyComponent,
+    CompaniesListComponent,
+    CompanyDetailComponent
   ],
   imports: [
     BrowserModule,
